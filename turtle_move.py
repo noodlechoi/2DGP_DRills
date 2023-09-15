22c8c183a4a3e0da89c0e7662d6cc9dbef7c87cb
minachoi <choime321@gmail.com>
2023-09-15 22:40:19 +0900

    add turtle stamp

--- turtle_move.py
+++ turtle_move.py
@@ -2,18 +2,22 @@
 import turtle
 
 def up_move():
+    turtle.stamp()
     turtle.setheading(90)   # 북쪽으로
     turtle.forward(50)
 
 def down_move():
+    turtle.stamp()
     turtle.setheading(270)   # 남쪽으로
     turtle.forward(50)
     
 def left_move():
+    turtle.stamp()
     turtle.setheading(180)   # 서쪽으로
     turtle.forward(50)
 
 def right_move():
+    turtle.stamp()
     turtle.setheading(0)   # 동쪽으로
     turtle.forward(50)
 
