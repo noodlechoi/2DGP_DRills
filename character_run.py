a0c88ea444cbcc41c03880890cdf0a46917ad516
minachoi <choime321@gmail.com>
2023-09-20 17:59:23 +0900

    complete circle function

--- character_run.py
+++ character_run.py
@@ -8,10 +8,15 @@ character = load_image('character.png')
 
 def run_circle():
     print('CIRCLE')
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(400, 90)
-    pass
+    cx, cy, r = 800 / 2, 600 / 2, 200
+    for deg in range(0, 360, 5):
+        x = cx + r * math.cos(deg / 360 * 2 * math.pi)
+        y = cy + r * math.sin(deg / 360 * 2 * math.pi)
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        delay(0.1)
+
 
 def run_rectangle():
     print('RECTANGLE')
