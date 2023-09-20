c673f803d38a2de2297eedb853daf74bcf07d6c2
minachoi <choime321@gmail.com>
2023-09-20 17:43:46 +0900

    complete circle and rectangle function frame

--- character_run.py
+++ character_run.py
@@ -1,7 +1,22 @@
 from pico2d import *
+import math
 
 open_canvas()
 
-# fill here
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+def run_circle():
+    print('CIRCLE')
+    pass
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+while True:
+    run_circle()
+    run_rectangle()
+    break;
 
 close_canvas()
