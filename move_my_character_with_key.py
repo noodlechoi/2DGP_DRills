4ee17c868613fd459f2c633f68e901dee2c65f3c
minachoi <choime321@gmail.com>
2023-09-26 01:59:37 +0900

    implement move and frame
    frame moves only when moving

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,13 +6,30 @@ ground = load_image('TUK_GROUND.png')
 character = load_image('skell.png')
 cell_size = 64
 
+def handle_events():
+    pass
+
 running = True
+x = WIDTH // 2
+y = HEIGHT // 2
+frame = {'x' : 0, 'y' : 0}
+dir = {'x' : 0, 'y' : 0}
 
 while running:
+    # 그리기
     clear_canvas()
     ground.draw(WIDTH // 2, HEIGHT // 2)
-    character.clip_draw(0, 0, cell_size, cell_size, WIDTH // 2, HEIGHT // 2)
+    character.clip_draw(frame['x'] * cell_size, frame['y'] * cell_size, cell_size, cell_size, x, y)
     update_canvas()
-    delay(0.01)
+    # 입력
+    handle_events()
+    # 이동
+    x += dir['x'] * 5
+    y += dir['y'] * 5
+
+    if(dir['x'] != 0 or dir['y'] != 0):
+        frame['x'] = (frame['x'] + 1) % 9
+    
+    delay(0.05)
 
 close_canvas()
