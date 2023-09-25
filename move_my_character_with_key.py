87c027330bbca497fd2c8e432b18f26b0dab8b1f
minachoi <choime321@gmail.com>
2023-09-26 01:52:54 +0900

    draw image

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -4,7 +4,15 @@ WIDTH, HEIGHT = 1280, 1024
 open_canvas(WIDTH, HEIGHT)
 ground = load_image('TUK_GROUND.png')
 character = load_image('skell.png')
+cell_size = 64
 
+running = True
 
+while running:
+    clear_canvas()
+    ground.draw(WIDTH // 2, HEIGHT // 2)
+    character.clip_draw(0, 0, cell_size, cell_size, WIDTH // 2, HEIGHT // 2)
+    update_canvas()
+    delay(0.01)
 
 close_canvas()
