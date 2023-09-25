7bbe0d2f2ffc37b86160e787313e87bd071e62c2
minachoi <choime321@gmail.com>
2023-09-26 02:04:56 +0900

    add key event quit

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -7,7 +7,16 @@ character = load_image('skell.png')
 cell_size = 64
 
 def handle_events():
-    pass
+    global running, x, y, dir, frame
+
+    events = get_events()
+    for event in events:
+        # 프로그램 종료
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
 
 def character_move():
     global x, y, dir, frame
