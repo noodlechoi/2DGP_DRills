17c5255422e51358c113427dbe5308ab484650fa
minachoi <choime321@gmail.com>
2023-09-27 18:52:36 +0900

    import random and load hand

--- Lecture07_Linear_Movement/move_character_with_mouse.py
+++ Lecture07_Linear_Movement/move_character_with_mouse.py
@@ -1,11 +1,12 @@
 from pico2d import *
+from random import *
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
-
+hand = load_image('hand_arrow.png')
 
 def handle_events():
     global running
