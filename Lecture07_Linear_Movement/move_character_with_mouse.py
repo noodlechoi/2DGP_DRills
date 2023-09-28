d681f9f1e7fbdfe69b5624820ff009b3ae08cbda
minachoi <choime321@gmail.com>
2023-09-28 14:48:46 +0900

    delete mouse motion and add draw line, but it's static

--- Lecture07_Linear_Movement/move_character_with_mouse.py
+++ Lecture07_Linear_Movement/move_character_with_mouse.py
@@ -17,8 +17,8 @@ def handle_events():
         # 프로그램 종료
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, HEIGHT - 1 - event.y
+        # elif event.type == SDL_MOUSEMOTION:
+        #     x, y = event.x, HEIGHT - 1 - event.y
 
 
 def character_move():
@@ -39,6 +39,23 @@ def character_move():
     if (dir['x'] != 0 or dir['y'] != 0):
         frame['x'] = (frame['x'] + 1) % 9
 
+def draw_line(target):
+    global x, y
+
+    for i in range(0, 100 + 1, 10):
+        # 이동 계산
+        t = i / 100
+        x = (1-t)*x + t*target[0]
+        y = (1-t)*y + t*target[1]
+        
+        # 그리기
+        clear_canvas()
+        ground.draw(WIDTH // 2, HEIGHT // 2)
+        hand.draw(target[0], target[1], cell_size, cell_size)
+        character.clip_draw(frame['x'] * cell_size, frame['y'] * cell_size, cell_size, cell_size, x, y, cell_size * 2,
+                            cell_size * 2)
+        update_canvas()
+        delay(0.1)
 
 running = True
 x = WIDTH // 2
@@ -47,21 +64,16 @@ frame = {'x': 0, 'y': 0}
 dir = {'x': 0, 'y': 0}
 target = [randint(0, WIDTH),randint(0, HEIGHT) ]
 
+hide_cursor()
 while running:
     # 그리기
-    clear_canvas()
-    ground.draw(WIDTH // 2, HEIGHT // 2)
-    hand.draw(target[0], target[1], cell_size, cell_size)
-    character.clip_draw(frame['x'] * cell_size, frame['y'] * cell_size, cell_size, cell_size, x, y, cell_size * 2,
-                        cell_size * 2)
-    update_canvas()
-    # 입력
+    draw_line(target)
+
+    # 키입력
     handle_events()
-    # character_move()
     # 랜덤 좌표 생성
     target = [randint(0, WIDTH),randint(0, HEIGHT) ]
 
-    delay(0.05)
 
 close_canvas()
 
