c56c0657aed017ca48e1c09be32caa8e18443c07
minachoi <choime321@gmail.com>
2023-09-28 15:30:53 +0900

    change sprite sheet and that target is mouse input

--- character_move_mouse_click.py
+++ character_move_mouse_click.py
@@ -9,15 +9,15 @@ hand = load_image('hand_arrow.png')
 cell_size = 64
 
 def handle_events():
-    global running, x, y, dir, frame
+    global running, target
 
     events = get_events()
     for event in events:
         # 프로그램 종료
         if event.type == SDL_QUIT:
             running = False
-        # elif event.type == SDL_MOUSEMOTION:
-        #     x, y = event.x, HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEMOTION:
+            target[0], target[1] = event.x, HEIGHT - 1 - event.y
 
 def character_move():
     global x, y, dir, frame
@@ -82,17 +82,14 @@ x = WIDTH // 2
 y = HEIGHT // 2
 frame = {'x': 0, 'y': 0}
 dir = {'x': 0, 'y': 0}
-target = [randint(0, WIDTH),randint(0, HEIGHT) ]
+target = [0, 0]
 
 # hide_cursor()
 while running:
-    # 그리기
-    move_line(target)
-
     # 키입력
     handle_events()
-    # 랜덤 좌표 생성
-    target = [randint(0, WIDTH),randint(0, HEIGHT) ]
+    # 그리기
+    move_line(target)
 
 close_canvas()
 
