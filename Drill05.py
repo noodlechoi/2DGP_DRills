283bf0e2a4064b46645745ac4ad60f6c5e264ba9
noodlechoi <choime321@gmail.com>
2023-10-03 20:07:33 +0900

    move randomly direct check using action

--- Drill05.py
+++ Drill05.py
@@ -26,26 +26,37 @@ def handle_events():
 
 def reset_world():
     global running, cx, cy, frame
-    global hx, hy
+    global hx, hy, sx, sy
+    global t, action
     running = True
     cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
+    action = 3
 
-    # hx, hy = TUK_WIDTH - 50, TUK_HEIGHT - 50
-    hx, hy = random.randint(0, TUK_HEIGHT), random.randint(0, TUK_HEIGHT)
-
+    sx, sy = cx, cy # p1
+    # hx, hy = 50, 50
+    hx, hy = random.randint(0, TUK_HEIGHT), random.randint(0, TUK_HEIGHT)   # p2
+    t = 0.0
 
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     arrow.draw(hx, hy)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, cx, cy)
+    character.clip_draw(frame * 100, 100 * action, 100, 100, cx, cy)
     update_canvas()
 
 
 def update_world():
     global frame
+    global cx, cy, t
+    global action
     frame = (frame + 1) % 8
+    action = 1 if cx < hx else 0
+
+    if t <= 1.0:
+        cx = (1-t)*sx + t*hx
+        cy = (1-t)*sy + t*hy
+        t += 0.001
 
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
