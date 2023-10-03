898d4048e445b399cb4e44a6e1e5703768f91e38
noodlechoi <choime321@gmail.com>
2023-10-03 20:38:00 +0900

    implement moving character to targets

--- Drill06.py
+++ Drill06.py
@@ -47,12 +47,20 @@ def set_new_target_arrow():
     global sx, sy, hx, hy, t
     global action
     global frame
-    sx, sy = cx, cy  # p1
-    # hx, hy = 50, 50
-    hx, hy = points[0]  # p2
-    t = 0.0
-    action = 1 if sx < hx else 0
-    frame = 0
+    global target_exists
+
+    if points:
+        sx, sy = cx, cy  # p1
+        # hx, hy = 50, 50
+        hx, hy = points[0]  # p2
+        t = 0.0
+        action = 1 if sx < hx else 0
+        frame = 0
+        target_exists = True
+    else:
+        action = 3 if action == 1 else 2
+        frame = 0
+        target_exists = False
 
 
 def render_world():
@@ -70,13 +78,15 @@ def update_world():
     global cx, cy, t
     frame = (frame + 1) % 8
 
-    if t <= 1.0:
-        cx = (1 - t) * sx + t * hx
-        cy = (1 - t) * sy + t * hy
-        t += 0.001
-    else:
-        cx, cy = hx, hy
-        set_new_target_arrow()
+    if target_exists:
+        if t <= 1.0:
+            cx = (1 - t) * sx + t * hx
+            cy = (1 - t) * sy + t * hy
+            t += 0.001
+        else:   # 목표에 도달 시
+            cx, cy = hx, hy
+            del points[0]
+            set_new_target_arrow()
 
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
