9a5c022b2f15b800b6a8a01eb7f9073d6bf3d89f
noodlechoi <choime321@gmail.com>
2023-10-04 18:55:26 +0900

    make grass and boy class

--- Lecture09_Game_Objects/boy_grass_object.py
+++ Lecture09_Game_Objects/boy_grass_object.py
@@ -1,6 +1,29 @@
 from pico2d import *
+import random
 
 # Game object class here
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    def update(self):   pass
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
 
 def handle_events():
     global running
@@ -11,11 +34,39 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+def reset_world():
+    global running
+    global grass
+    global boy
+    global team
+
+    running = True
+    grass = Grass()
+    team = [Boy() for _ in range(10)]
+
+def update_world():
+    grass.update()
+    for boy in team:
+        boy.update()
+
+def render_world():
+    clear_canvas()
+    grass.draw()
+    for boy in team:
+        boy.draw()
+    update_canvas()
+
 open_canvas()
 
 # initialization code
+reset_world()
 
 # game main loop code
+while running:
+    handle_events()
+    update_canvas() # game logic
+    render_world()  # draw world
+    delay(0.05)
 
 # finalization code
 
