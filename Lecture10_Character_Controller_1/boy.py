77d07cc909614ad8b036b3a1a9c9f1c8d140dded
noodlechoi <choime321@gmail.com>
2023-10-11 17:42:22 +0900

    add file

---
+++ Lecture10_Character_Controller_1/boy.py
@@ -0,0 +1,59 @@
+from pico2d import load_image
+
+
+class Idle:
+    @staticmethod
+    def enter():
+        print('IDLE Entered - 고개 숙이기')
+        pass
+
+    @staticmethod
+    def exit():
+        print('IDLE Exit')
+        pass
+
+    @staticmethod
+    def do():
+        print('드르렁')
+        pass
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+        pass
+
+class StateMachine():
+    def __init__(self):
+        # 생성 X, idle 그룹
+        self.cur_state = Idle
+        pass
+    def start(self):
+        self.cur_state.enter()
+        pass
+    def update(self):
+        self.cur_state.do()
+        pass
+    def draw(self):
+        self.cur_state.draw()
+        pass
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 400, 90
+        self.frame = 0
+        self.dir = 0
+        self.action = 3
+        self.image = load_image('animation_sheet.png')
+        self.state_machine = StateMachine()
+        self.state_machine.start()
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.state_machine.update()
+
+    def handle_event(self, event):
+        pass
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, self.action * 100, 100, 100, self.x, self.y)
+        self.state_machine.draw()
