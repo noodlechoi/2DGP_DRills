a3b2f3e791b08fee9053aade455426a734fdeb2e
noodlechoi <choime321@gmail.com>
2023-10-18 18:30:25 +0900

    lecture 12 complete

--- Lecture12_Game_World/boy.py
+++ Lecture12_Game_World/boy.py
@@ -2,6 +2,10 @@
 
 from pico2d import get_time, load_image, SDL_KEYDOWN, SDL_KEYUP, SDLK_SPACE, SDLK_LEFT, SDLK_RIGHT
 
+from Lecture12_Game_World import game_world
+from ball import Ball
+
+
 # state event check
 # ( state event type, event value )
 
@@ -45,7 +49,8 @@ class Idle:
 
     @staticmethod
     def exit(boy, e):
-        pass
+        if space_down(e):
+            boy.fire_ball()
 
     @staticmethod
     def do(boy):
@@ -70,7 +75,8 @@ class Run:
 
     @staticmethod
     def exit(boy, e):
-        pass
+        if space_down(e):
+            boy.fire_ball()
 
     @staticmethod
     def do(boy):
@@ -114,8 +120,8 @@ class StateMachine:
         self.boy = boy
         self.cur_state = Idle
         self.transitions = {
-            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep},
-            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
+            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep, space_down : Idle},
+            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, space_down : Run},
             Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle}
         }
 
@@ -161,3 +167,12 @@ class Boy:
 
     def draw(self):
         self.state_machine.draw()
+
+    def fire_ball(self):
+        ball = Ball(self.x, self.y, self.face_dir * 10)
+        game_world.add_object(ball, 1)
+
+        if self.face_dir == -1:
+            print('FIRE BALL to LEFT')
+        elif self.face_dir == 1:
+            print('FIRE BALL to RIGHT')
