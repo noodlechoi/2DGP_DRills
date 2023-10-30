2f68c35554c5c5cf4e21fbf7b850c3365aa87aae
noodlechoi <choime321@gmail.com>
2023-10-30 16:03:20 +0900

    lecture 10/30

--- Lecture13_Game_Framework/game_world.py
+++ Lecture13_Game_Framework/game_world.py
@@ -1,6 +1,5 @@
 objects = [[] for _ in range(4)]
 
-
 def add_object(o, depth = 0):
     objects[depth].append(o)
 
@@ -26,3 +25,9 @@ def remove_object(o):
             layer.remove(o)
             return
     raise ValueError('Cannot delete non existing object')
+
+
+def clear():
+    for layer in objects:
+        layer.clear()
+    pass
---
+++ Lecture13_Game_Framework/play_mode.py
@@ -0,0 +1,55 @@
+from pico2d import *
+
+import game_world
+import game_framework
+import title_mode
+from grass import Grass
+from boy import Boy
+import item_mode
+
+# Game object class here
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_mode(title_mode)
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_i:
+            game_framework.push_mode(item_mode)
+        else:
+            boy.handle_event(event)
+
+def init():
+    global running
+    global grass
+    global boy
+
+    running = True
+
+    grass = Grass()
+    game_world.add_object(grass, 0)
+
+    boy = Boy()
+    game_world.add_object(boy, 1)
+
+
+def finish():
+    game_world.clear()
+    pass
+
+def update():
+    game_world.update()
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def pause():
+    pass
+
+def resume():
+    pass
