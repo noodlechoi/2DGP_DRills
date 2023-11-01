2ad3b845a7cf08b62efa2b1a723cf6b673fea4da
noodlechoi <choime321@gmail.com>
2023-11-01 19:51:57 +0900

    Drill11 제출 (2022180039 최미나)

--- Lecture14_Time/bird.py
+++ Lecture14_Time/bird.py
@@ -8,11 +8,11 @@ RUN_SPEED_MPM = RUN_SPEED_KMPH * 1000.0 / 60.0
 RUN_SPEED_MPS = RUN_SPEED_MPM / 60.0
 RUN_SPEED_PPS = RUN_SPEED_MPS * PIXEL_PER_METER
 
-TIME_PER_ACTION = 0.5
+TIME_PER_ACTION = 0.1
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
-FRAMES_PER_ATCION = 14
+FRAMES_PER_ACTION = 13
 
-FRAMES_PER_TIME = ACTION_PER_TIME * FRAMES_PER_ATCION
+FRAMES_PER_TIME = ACTION_PER_TIME * FRAMES_PER_ACTION
 
 class Bird():
     image = None
@@ -30,7 +30,7 @@ class Bird():
             Bird.image = load_image('bird_animation.png')
 
     def update(self):
-        self.frame = int(self.frame + FRAMES_PER_ATCION * ACTION_PER_TIME * game_framework.frame_time) % 14
+        self.frame = int(self.frame + FRAMES_PER_TIME * ACTION_PER_TIME * game_framework.frame_time) % 14
         self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time
 
         self.frame_x = self.frame % 5
